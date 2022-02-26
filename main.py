cbc802a0e503f33f1676d536a06e0c9ab4c011c8
Emeritus <jabridge2552@gmail.com>
2022-02-26 12:49:47 -0700

    changed into a class and changed drive function names

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
-
-def move_forward():
-    print("move forward")
-    return
-
-
-def move_left():
-    print("turn left")
-    return
-
-
-def move_back():
-    print("move backward")
-    return
-
-
-def move_right():
-    print("turn right")
-    return
-
-
-if __name__ == '__main__':
-    command = 1
-    while command != '0':
-        print('Enter command:')
-        command = input()
-        if command == 'w':
-            move_forward()
-        elif command == 'a':
-            move_left()
-        elif command == 's':
-            move_back()
-        elif command == 'd':
-            move_right()
-        else:
-            continue
-    print("goodbye")
+class TangoBot:
+    # These are the main movement controls    
+    def drvForward(self):
+        print("move forward")
+        return
+    
+    
+    def drvRotateLeft(self):
+        print("turn left")
+        return
+    
+    
+    def drvBack(self):
+        print("move backward")
+        return
+    
+    
+    def drvRotateRigth(self):
+        print("turn right")
+        return
+    
+    
+    if __name__ == '__main__':
+        command = 1
+        while command != '0':
+            print('Enter command:')
+            command = input()
+            if command == 'w':
+                move_forward()
+            elif command == 'a':
+                move_left()
+            elif command == 's':
+                move_back()
+            elif command == 'd':
+                move_right()
+            else:
+                continue
+        print("goodbye")
 
 
