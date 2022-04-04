0d291eaca23e30c8a1feca8e12451fd330efdc36
Emeritus <jabridge2552@gmail.com>
2022-04-04 13:17:46 -0600

    updated data structure

--- convo.py
+++ convo.py
@@ -1,3 +1,10 @@
+# This class creates the data structure of answers/subanswers
+class Dialogue:
+    question=None # question to respond to
+    response=None # answer to the question, array for multiple options
+    subrules=None # Array of more sub-dialogue options
+    pass
+
 class Convo:
     responses=None #list of listparsed U responses
     valid=None #list of valid responses
