05885cec53e53c279baa34af3429451469cf3ca9
Emeritus <jabridge2552@gmail.com>
2022-04-04 10:11:13 -0600

    added outline of class

---
+++ convo.py
@@ -0,0 +1,14 @@
+class Convo:
+    responses=None #list of listparsed U responses
+    valid=None #list of valid responses
+
+    def parse(self, inp):
+        # add in string parsing, remove whitespace, tolower, etc
+        pass
+
+    def ask(self,inp):
+        passd
+        # Modify the valid list each time this is called
+
+# load in test file and test this class
+
