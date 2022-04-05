3dc31a68336e4323765cc464a60c694acea993f1
Emeritus <jabridge2552@gmail.com>
2022-04-05 17:54:39 -0600

    Updated data struct, added comments

--- convo.py
+++ convo.py
@@ -3,11 +3,21 @@ class Dialogue:
     question=None # question to respond to
     response=None # answer to the question, array for multiple options
     subrules=None # Array of more sub-dialogue options
-    pass
+    
+    def __init__(self, questionStr, answerStr="I'm broken!", subrulesList=[]):
+        question=questionStr
+        response=answerStr
+        subrules=subrulesList
 
+# This class contains methods to parse a file into a question/response data structure,
+# and query that data structure with questions and keep record of valid reponses at
+# any given time. 
 class Convo:
-    responses=None #list of listparsed U responses
-    valid=None #list of valid responses
+    responses=None #list of Dialogue structures
+    valid=None #list of valid questions/Dialogue structures to respond to.
+
+    def __init__(self):
+        pass
 
     def parse(self, inp):
         # add in string parsing, remove whitespace, tolower, etc
